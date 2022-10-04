6d8e53a70547a24281ee74e96303f3e5341b1d2d
alfonsete23 <alfonso.navarrog@um.es>
2022-10-04 21:09:16 +0200

    Finished game Add checking for ties Add input control Add game loop

--- game.rb
+++ game.rb
@@ -9,6 +9,7 @@ class Board
             [7, 8, 9]
         ]
         @delim = "---+---+---"
+        @turns = 0
     end
 
     public
@@ -19,16 +20,28 @@ class Board
             puts " #{@board[row][0]} | #{@board[row][1]} | #{@board[row][2]}"
             puts "#{@delim}"
         end
+        puts
     end
 
     # Change the symbol in the provided position
     def move(symbol, position)
+        # If position is not between 1 and 9 return false
+        unless position.between?(1, 9)
+            return false
+        end
         # Get row
         row = ((position - 1) / 3).to_i
         # Get column
         col = (position - 1) % 3
-        # Set move on the board
-        @board[row][col] = symbol
+        # Check if move is valid
+        if move_valid?(row, col)
+            # Set move on the board
+            @board[row][col] = symbol
+            @turns += 1
+            return true
+        end
+        # If move is not valid, return false
+        return false
     end
 
     # Check the winner using the helper functions defined to check rows, columns and diagonals
@@ -39,6 +52,8 @@ class Board
             return winner
         elsif winner = check_diagonals
             return winner
+        elsif @turns == 9
+            return "tie"
         else
             return
         end
@@ -86,18 +101,65 @@ class Board
             return
         end
     end
+
+    # Check if a move is valid
+    def move_valid?(row, col)
+        # Check if the value in the position is a number
+        if (@board[row][col].is_a?(Numeric))
+            return true
+        else
+            return false
+        end
+    end
 end
 
+# Create board to play
 game = Board.new
-game.check_winner
+# Game loop
 while true do
+    # Clear the terminal
+    puts `clear`
+    # Display the board
     game.display_board
-    puts "Make your move"
-    move = gets.to_i
-    game.move("X", move)
+        # Ask player X for their move
+        puts "Player X make your move"
+        move = gets.to_i
+        # Make the move, making sure it is a valid move
+        until game.move("X", move) do
+            # Ask player X for their move
+            puts "Please, player X, make a valid move"
+            move = gets.to_i
+        end
+    # Check if there is a winner after the move
     if winner = game.check_winner
         game.display_board
-        puts "#{winner} wins!"
+        if winner = "tie"
+            puts "Tied game!"
+        else
+            puts "#{winner} wins!"
+        end    
         break
+    end
+    # Clear the terminal
+    puts `clear`
+    # Display the board
+    game.display_board
+    # Ask player O for their move
+    puts "Player O make your move"
+    move = gets.to_i
+    # Make the move, making sure it is a valid move
+    until game.move("O", move) do
+        # Ask player X for their move
+        puts "Please, player O, make a valid move"
+        move = gets.to_i
+    end
+    # Check if there is a winner after the move
+    if winner = game.check_winner
+        game.display_board
+        if winner = "tie"
+            puts "Tied game!"
+        else
+            puts "#{winner} wins!"
+        end 
     end
 end
