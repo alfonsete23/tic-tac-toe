11738fcb079dab55657c6682374092e9dcfb3be7
alfonsete23 <alfonso.navarrog@um.es>
2022-10-04 20:40:58 +0200

    Add check_winner function Add check_rows, check_columns and check_diagonals to help check_winner

--- game.rb
+++ game.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 class Board
     # Initialize a board with 3 rows of 3 positions
     def initialize
@@ -10,7 +12,7 @@ class Board
     end
 
     public
-    # Print the board TODO: look for a cleaner way of doing this
+    # Print the board
     def display_board
         puts "#{@delim}"
         (0..2).each do |row|
@@ -29,12 +31,73 @@ class Board
         @board[row][col] = symbol
     end
 
-    #TODO
+    # Check the winner using the helper functions defined to check rows, columns and diagonals
     def check_winner
-        
+        if winner = check_rows
+            return winner
+        elsif winner = check_columns
+            return winner
+        elsif winner = check_diagonals
+            return winner
+        else
+            return
+        end
+    end
+
+    private
+    # Turn rows into sets and check if their length is 1    
+    def check_rows
+        @board.each do |row|
+            if row.to_set.length == 1
+                return row[0]
+            else
+                return
+            end
+        end
+    end
+
+    # Turn columns into sets and check if their length is 1
+    def check_columns
+        (0..2).each do |i|
+            # Create array for the column to check
+            col = []
+            (0..2).each do |j|
+                col.push(@board[j][i])
+            end
+            # Check like previously, turning it into a set and checking if its length is 1
+            if col.to_set.length == 1
+                return col[0]
+            end 
+        end
+        return
+    end
+
+    #Turn diagonals into sets and check if their length is 1
+    def check_diagonals
+        # Initialize diagonals
+        d1 = [@board[0][0], @board[1][1], @board[2][2]]
+        d2 = [@board[0][2], @board[1][1], @board[2][0]]
+        # Turn them into sets and check if length is 1
+        if d1.to_set.length == 1
+            return d1[0]
+        elsif d2.to_set.length == 1
+            return d2[0]
+        else
+            return
+        end
     end
 end
 
 game = Board.new
-game.move("X", 9)
-game.display_board
+game.check_winner
+while true do
+    game.display_board
+    puts "Make your move"
+    move = gets.to_i
+    game.move("X", move)
+    if winner = game.check_winner
+        game.display_board
+        puts "#{winner} wins!"
+        break
+    end
+end
